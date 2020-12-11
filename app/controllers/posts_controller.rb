e0f963d1ba27caff31e7b07f42208f32a10807d5
sayaka0915 <sayaka0915@users.noreply.github.com>
2020-12-11 20:21:59 +0900

    更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index 
     @posts = Post.all.order(id: "DESC")
+    @post_count = @posts.count
   end
 
   def create
---
+++ app/javascript/count.js
@@ -0,0 +1,9 @@
+document.getElementById('submit').onclick = function() {
+      const commentView = document.getElementsByClassName('comment');
+      const commentNum = commentView.length;
+      console.log(commentNum );
+
+      // const commentCount = document.getElementById('comment-count');
+      // commentCount.innerHTML = `${commentNum}件` 
+    }
+  
